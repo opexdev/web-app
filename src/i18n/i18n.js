fcc1277a773cf87571acd26d67d06519029c5bc1
benyamin001 <azimi20022003@yahoo.com>
2021-01-09 14:55:11 +0330

    persist language

--- src/i18n/i18n.js
+++ src/i18n/i18n.js
@@ -1,16 +1,24 @@
 import i18n from 'i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
 import Backend from 'i18next-http-backend';
 
 
+const options = {
+    order: ['localStorage'],
+    lookupLocalStorage: 'language',
+    caches: ['localStorage'],
+}
+
 i18n
+    .use(LanguageDetector)
     .use(Backend)
     .use(initReactI18next)
     .init({
-        lng: "fa",
         preload : ["fa"],
         fallbackLng: 'fa',
         debug: true,
+        detection: options,
         interpolation: {
             escapeValue: false,
         }
