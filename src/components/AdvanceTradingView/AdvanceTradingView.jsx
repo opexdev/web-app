b03151e3c1e542075a814199f9a4d1ec165f857c
metalicn20 <metalicn20@gmail.com>
2021-05-19 12:10:12 +0430

    Mock searching symbols in advance trading view

--- src/components/AdvanceTradingView/AdvanceTradingView.jsx
+++ src/components/AdvanceTradingView/AdvanceTradingView.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useLayoutEffect} from "react";
+import React, {useLayoutEffect} from "react";
 import DataFeed from "./DataFeed";
 
 const widgetOptions = {
@@ -11,35 +11,12 @@ const widgetOptions = {
   interval: "1H",
   locale: "fa",
   drawings_access: {type: "black", tools: [{name: "Regression Trend"}]},
-  disabled_features: [
-    // "use_localstorage_for_settings",
-    // "header_widget",
-    // "volume_force_overlay",
-    // "timeframes_toolbar",
-  ],
-  enabled_features: ["study_templates"],
+  disabled_features: [],
+  enabled_features: [],
   timezone: Intl.DateTimeFormat().resolvedOptions().timeZone,
   theme: "light",
   width: "100%",
   height: "100%",
-  // toolbar_bg: "#171b2b",
-  // loading_screen: {
-  //   backgroundColor: "#171b2b",
-  // },
-  // studies_overrides: {
-  //   "volume.volume.color.0": "#e0294a",
-  //   "volume.volume.color.1": "#2ebd85",
-  //   "volume.volume.transparency": 65,
-  // },
-  // overrides: {
-  //   "paneProperties.background": "#171b2b",
-  //   "mainSeriesProperties.candleStyle.upColor": "#2ebd85",
-  //   "mainSeriesProperties.candleStyle.downColor": "#e0294a",
-  //   "mainSeriesProperties.candleStyle.wickUpColor": "#2ebd85",
-  //   "mainSeriesProperties.candleStyle.wickDownColor": "#e0294a",
-  //   "mainSeriesProperties.candleStyle.borderUpColor": "#2ebd85",
-  //   "mainSeriesProperties.candleStyle.borderDownColor": "#e0294a",
-  // },
 };
 
 const AdvanceTradingView = (props) => {
