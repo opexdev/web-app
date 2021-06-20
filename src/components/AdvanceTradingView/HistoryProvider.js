63516d9dd7ec6b0f5b7460b29e569da94b34c23a
metalicn20 <metalicn20@gmail.com>
2021-06-20 10:35:22 +0430

    Implement hour interval

--- src/components/AdvanceTradingView/HistoryProvider.js
+++ src/components/AdvanceTradingView/HistoryProvider.js
@@ -2,12 +2,15 @@ const api_root = "https://api.binance.com/api/v3/klines";
 const history = {};
 
 export default {
-  history: history,
+  history,
   async getBars(symbolInfo, resolution, from, to, first, limit) {
+    const interval = Number(resolution.toLowerCase())
+      ? `${Number(resolution.toLowerCase()) / 60}h`
+      : resolution.toLowerCase();
     const symbol = symbolInfo.name.replace("/", "");
     const url = `${api_root}?symbol=${symbol}&startTime=${
       from * 1000
-    }&endTime=${to * 1000}&interval=${resolution.toLowerCase()}`;
+    }&endTime=${to * 1000}&interval=${interval}`;
     return fetch(url).then(async (res) => {
       const data = await res.json();
       if (res.status !== 200) {
