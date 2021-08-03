e33c5e355768c1dee9fcccaa51d25ae3a4e7db13
Benyamin001 <23664159+Benyamin001@users.noreply.github.com>
2021-08-03 12:48:19 +0430

    close #8,TextInput component refactored

--- src/components/TextInput/TextInput.js
+++ src/components/TextInput/TextInput.js
@@ -1,59 +1,68 @@
+
 import React from "react";
 import Icon from "../Icon/Icon";
 import Select from "react-select";
 import * as classes from "./TextInput.module.css";
 
 const TextInput = (props) => {
-  return (
-    <div className={props.customClass ?? ""}>
-      <div className={classes.inputGroup}>
-        {props.lead ? (
-          <span className={`lead ${classes.lead}`}>{props.lead}</span>
-        ) : (
-          ""
-        )}
-        {props.select ? (
-          <Select
-            onChange={props.onchange}
-            options={props.options}
-            placeholder={props.placeholder}
+    const {customRef,readOnly,onchange,customClass,options, lead , after ,select ,alerts , ...other} = props
+
+    let leadSection = null
+    let afterSection = null
+    let alertSection = null
+
+    let inputSection = <input
+        ref={customRef}
+        readOnly={readOnly}
+        onChange={onchange}
+        {...other}
+    />
+
+    if(lead){
+        leadSection = <span className={`lead ${classes.lead}`}>{lead}</span>
+    }
+
+    if ( select ){
+        inputSection = <Select
+            onChange={onchange}
+            options={options}
             classNamePrefix="select"
             className={classes.selectBox}
-          />
-        ) : (
-          <input
-            value={props.value}
-            ref={props.customRef}
-            type={props.type}
-            readOnly={props.readOnly}
-            onChange={props.onchange}
-            placeholder={props.placeholder}
-          />
-        )}
-        {props.after ? (
-          <span className={`after ${classes.after}`}>{props.after}</span>
-        ) : (
-          ""
-        )}
-      </div>
-      {props.alert != null ? (
-        <div
-          className={`${classes.inputGroupHint} inputGroupHint `}
-          data-html={true}
-          data-place="left"
-          data-effect="float"
-          data-tip={props.hint}>
-          <Icon
-            iconName={`${classes.iconInfo} icon-white font-size-sm flex`}
-            customClass="hint-icon"
-          />
-          <span className={`${classes.alert} pr-05 `}>{props.alert}</span>
+            {...other}
+        />
+    }
+    if(after){
+        afterSection = <span className={`after ${classes.after}`}>{after}</span>
+    }
+
+    if (alerts){
+        alertSection =<div
+            className={`${classes.inputGroupHint} inputGroupHint `}
+            data-html={true}
+            data-place="left"
+            data-effect="float"
+            data-tip={props.hint}>
+            <Icon
+                iconName={`${classes.iconInfo} icon-white font-size-sm flex`}
+                customClass="hint-icon"
+            />
+            <div className="column pt-05">
+                { alerts.map((alert , index) => <span key={index} className={`${classes.alert} pr-05 `}>{alert}</span>) }
+            </div>
         </div>
-      ) : (
-        ""
-      )}
+    }
+
+    return (
+    <div className={customClass ?? ""}>
+      <div className={classes.inputGroup}>
+          {leadSection}
+          {inputSection}
+          {afterSection}
+      </div>
+        {alertSection}
     </div>
   );
 };
+
 
 export default TextInput;
