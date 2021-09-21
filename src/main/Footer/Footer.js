e7df9efac2eb89864eb57c8da7eefe733ad94082
Hossein_ab99 <hossein13785ab@gmail.com>
2021-09-21 12:52:09 +0430

    #31, add guide page

--- src/main/Footer/Footer.js
+++ src/main/Footer/Footer.js
@@ -5,91 +5,87 @@ import {useTranslation} from "react-i18next";
 import i18n from "../../i18n/i18n";
 import {connect} from "react-redux";
 import {setThemeInitiate} from "../../store/actions/";
-import {Link} from "react-router-dom";
-import {Guide} from "../../routes/routes";
+import  *  as Routes  from "../../routes/routes";
+import ToggleSwitch from "../../components/ToggleSwitch/ToggleSwitch";
+import {NavHashLink} from "react-router-hash-link";
 
 const Footer = (props) => {
-  const {t} = useTranslation();
-  return (
-    <div className={`container column footerBackground ${classes.container}`}>
-      <div className={`row jc-between ai-center ${classes.content}`}>
-        <div className="row">
-          <Link to={Guide} className="row">
-            <div className="column px-1">
-                <span className="hover-text">{t("footer.aboutUs")}</span>
-                <span className="hover-text">{t("footer.contactUS")}</span>
-                <span className="hover-text">{t("footer.blog")}</span>
-                <span className="hover-text">{t("footer.guide")}</span>
-                <span className="hover-text">{t("footer.rules")}</span>
-            </div>
-            <div className="column px-1">
-                <span className="hover-text">{t("footer.wage")}</span>
-                <span className="hover-text">{t("footer.api")}</span>
-                <span className="hover-text">{t("footer.addCoin")}</span>
-                <span className="hover-text">{t("footer.demo")}</span>
-                <span className="hover-text">{t("footer.errorReport")}</span>
-            </div>
-          </Link>
-        </div>
+    const {t} = useTranslation();
+    return (
+        <div className={`container column footerBackground font-size-sm-plus mt-1 py-2 px-4`}>
+            <div className={`container row jc-between ai-center`}>
+                <div className="row">
 
-        <div className={`${classes.footerSetting} `}>
-          <div className={`row ai-center py-2`}>
-            <span className={`pl-1`}>{t("footer.changeColor")}:</span>
-            <div className={classes.onoffswitch}>
-              <input
-                type="checkbox"
-                name="onoffswitch"
-                className={classes.onoffswitchCheckbox}
-                id="myonoffswitch"
-                checked={props.isDark}
-                onChange={(e) => props.onThemeChange(e.target.checked)}
-              />
-              <label
-                className={classes.onoffswitchLabel}
-                htmlFor="myonoffswitch">
-                <span className={classes.onoffswitchInner} />
-                <span className={classes.onoffswitchSwitch} />
-              </label>
-            </div>
-          </div>
+                    <div className="column px-1">
+                        <NavHashLink to={Routes.Guide + "#about-us"}>
+                            <span className="hover-text">{t("footer.aboutUs")}</span>
+                        </NavHashLink>
+                        <NavHashLink to={Routes.Guide + "#contact-us"}>
+                            <span className="hover-text">{t("footer.contactUS")}</span>
+                        </NavHashLink>
+                        <NavHashLink to={Routes.Guide + "#blog"}>
+                            <span className="hover-text">{t("footer.blog")}</span>
+                        </NavHashLink>
+                        <NavHashLink to={Routes.Guide + "#guides"}>
+                            <span className="hover-text">{t("footer.guide")}</span>
+                        </NavHashLink>
+                        <NavHashLink to={Routes.Guide + "#rules"}>
+                            <span className="hover-text">{t("footer.rules")}</span>
+                        </NavHashLink>
+                    </div>
+                    <div className="column px-1">
+                        <NavHashLink to={Routes.Guide + "#wage"}>
+                            <span className="hover-text">{t("footer.wage")}</span>
+                        </NavHashLink>
+                        <NavHashLink to={Routes.Guide + "#api"}>
+                            <span className="hover-text">{t("footer.api")}</span>
+                        </NavHashLink>
+                        <NavHashLink to={Routes.Guide + "#addCoin"}>
+                            <span className="hover-text">{t("footer.addCoin")}</span>
+                        </NavHashLink>
+                        <NavHashLink to={Routes.Guide + "#demo"}>
+                            <span className="hover-text">{t("footer.demo")}</span>
+                        </NavHashLink>
+                        <NavHashLink to={Routes.Guide + "#errorReport"}>
+                            <span className="hover-text">{t("footer.errorReport")}</span>
+                        </NavHashLink>
+                    </div>
 
-          <div className={`row ai-center jc-between`}>
-            <div className={`row ai-center ${classes.languages}`}>
-              <span
-                className="cursor-pointer"
-                onClick={() => i18n.changeLanguage("fa")}>
-                فارسی
-              </span>
-              <span
-                className="cursor-pointer"
-                onClick={() => i18n.changeLanguage("en")}>
-                English
-              </span>
-            </div>
-          </div>
-        </div>
+                </div>
 
-        <div className={classes.footerLogo}>
-          <img src={images.opexLogo_light} alt="opexLogo_light" />
+                <div className={`column ai-center jc-center`}>
+                    <div className={`row ai-center py-2`}>
+                        <span className={`pl-1`}>{t("footer.changeColor")}:</span>
+                        <ToggleSwitch onchange={(e) => props.onThemeChange(e.target.checked)} checked={props.isDark}/>
+                    </div>
+
+                    <div className={`row ai-center jc-between`}>
+                        <div className={`row ai-center ${classes.languages}`}>
+                            <span className="cursor-pointer pl-1"
+                                  onClick={() => i18n.changeLanguage("fa")}>{t("Languages.Persian")}</span>
+                            <span className="cursor-pointer pr-1"
+                                  onClick={() => i18n.changeLanguage("en")}>{t("Languages.English")}</span>
+                        </div>
+                    </div>
+                </div>
+                <img className={`img-lg-1`} src={images.opexLogo_light} alt="opexLogo_light"/>
+            </div>
+            <div className={`container flex jc-center ai-center`}>
+                <p>{t("footer.copyright")}</p>
+            </div>
         </div>
-      </div>
-      <div className={classes.navbar}>
-        <p>{t("footer.copyright")}</p>
-      </div>
-      <img src="" alt="" />
-    </div>
-  );
+    );
 };
 
 const mapStateToProps = (state) => {
-  return {
-    isDark: state.global.isDark,
-  };
+    return {
+        isDark: state.global.isDark,
+    };
 };
 const mapDispatchToProps = (dispatch) => {
-  return {
-    onThemeChange: (isDark) => dispatch(setThemeInitiate(isDark)),
-  };
+    return {
+        onThemeChange: (isDark) => dispatch(setThemeInitiate(isDark)),
+    };
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Footer);
