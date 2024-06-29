3932c18baf3a9e974754882d335e5c2c72af2470
Hossein <hossein13785ab@gmail.com>
2024-06-29 13:53:13 +0330

    Update general info component

--- src/main/Browser/Pages/AllMarket/components/VolumeInfo/components/MostVolume/MostVolume.js
+++ src/main/Browser/Pages/AllMarket/components/VolumeInfo/components/MostVolume/MostVolume.js
@@ -19,9 +19,12 @@ const MostVolume = ({mostVolume}) => {
                 <span className={`${i18n.language !== "fa" ? 'mr-025' : 'ml-025'} fs-0-6`}>{mostVolume?.pairInfo?.baseAsset}</span>
                 <span className={`${i18n.language !== "fa" ? 'mL-025' : 'mr-025'} fs-01`}>{new BN(mostVolume?.volume).toFormat()} </span>
             </div>
-            <div className={`row jc-center ai-center width-100`}>
+
+            {/*++ to do ++*/}
+
+            {/*<div className={`row jc-center ai-center width-100`}>
                 <span  className={`${mostVolume?.change > 0 ? "text-green" : "text-red"} direction-ltr`}>{new BN(mostVolume?.change).toFormat(2)} %</span>
-            </div>
+            </div>*/}
         </>
     );
 };
