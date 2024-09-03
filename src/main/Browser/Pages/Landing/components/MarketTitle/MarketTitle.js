a5cab1a1915ca05c4fae67b03508244cbc94d132
Hossein <hossein13785ab@gmail.com>
2024-09-03 16:06:44 +0330

    Check env in market title comp

--- src/main/Browser/Pages/Landing/components/MarketTitle/MarketTitle.js
+++ src/main/Browser/Pages/Landing/components/MarketTitle/MarketTitle.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import classes from './MarketTitle.module.css'
 import {useTranslation} from "react-i18next";
-import {Panel} from "../../../../Routes/routes";
+import {EasyTrading, Panel} from "../../../../Routes/routes";
 import {Link} from "react-router-dom";
 import Icon from "../../../../../../components/Icon/Icon";
 import Button from "../../../../../../components/Button/Button";
@@ -9,6 +9,8 @@ import Button from "../../../../../../components/Button/Button";
 const MarketTitle = () => {
 
     const {t} = useTranslation();
+
+    const isDevelopment = window.env.REACT_APP_ENV === "development";
 
     const buttonTitle = <Icon iconName="icon-op-02 flex fs-04" customClass={`flex jc-center ai-center py-1`}/>
 
@@ -18,35 +20,23 @@ const MarketTitle = () => {
                 <span className={`text-orange fs-02`}>{t("title")}</span> {t("MarketTitle.content")}
             </p>
             <div className={`column`}>
-                {/*<div className={`row jc-start ai-center mb-1`}>
-                    <span className={`${classes.arrow} ml-05`}></span>
-                    <Link to={"#"} className={`mr-05 cursor-pointer hover-text hover-scale-01`}>{t("MarketTitle.easyTrading")}</Link>
-                </div>*/}
-                <div className={`row jc-start ai-center mt-1 ${classes.flashit}`}>
+                <div className={`row jc-start ai-center my-1 ${classes.flashit}`}>
                     <span className={`${classes.arrow} ml-05`}></span>
                     <Link to={Panel} className={`mr-05 cursor-pointer hover-text hover-scale-01`}>{t("MarketTitle.advancedTrading")}</Link>
                 </div>
-                {/*<Button
-                    buttonClass={`${classes.marketButton} ${classes.easyTradingButton} my-1`}
-                    type="button"
-                    onClick={() => navigate("", { replace: true })}
-                    buttonTitle={t("MarketTitle.easyTrading")}
-                />
-                <Button
-                    buttonClass={`${classes.marketButton} ${classes.advancedTradingButton} my-1`}
-                    type="button"
-                    onClick={() => navigate(Panel, { replace: true })}
-                    buttonTitle={t("MarketTitle.advancedTrading")}
-                />*/}
+                <div className={`row jc-start ai-center my-1`}>
+                    <span className={`${classes.arrow} ml-05`}></span>
+                    <Link to={EasyTrading} className={`mr-05 cursor-pointer hover-text hover-scale-01`}>{t("MarketTitle.easyTrading")}</Link>
+                </div>
             </div>
-            <div className={`row jc-start ai-center`}>
+            { isDevelopment && <div className={`row jc-start ai-center`}>
                 <Button
                     buttonClass={`${classes.thisButton} hover-text`}
                     type="button"
                     onClick={() => window.open(`https://github.com/opexdev`)}
                     buttonTitle={buttonTitle}
                 />
-            </div>
+            </div>}
         </div>
     );
 };
