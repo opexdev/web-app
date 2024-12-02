88ba6b5e9b0704e8c69f0180be96c9a07894f201
Hossein <hossein13785ab@gmail.com>
2024-12-02 20:05:09 +0330

    Handle various states for the MarketTitle component

--- src/main/Browser/Pages/Landing/components/MarketTitle/MarketTitle.js
+++ src/main/Browser/Pages/Landing/components/MarketTitle/MarketTitle.js
@@ -14,13 +14,28 @@ const MarketTitle = () => {
 
     const buttonTitle = <Icon iconName="icon-op-02 flex fs-04" customClass={`flex jc-center ai-center py-1`}/>
 
+    const campaignLink = t("Campaign.link");
+    const hasCampaign = t("Campaign.hasCampaign") === "true";
+
+
+    const clickHandler = () => {
+        if (campaignLink) {
+            window.open(campaignLink, "_blank");
+        } else {
+            console.error("Link not found");
+        }
+    }
+
     return (
         <div className={`${classes.container} column jc-between card-bg card-border px-2 py-3`}>
-            <p className={`fs-01`}>
+            { !hasCampaign ? <p className={`fs-01`}>
                 <span className={`text-orange fs-02`}>{t("title")}</span> {t("MarketTitle.content")}
-            </p>
+            </p> : <div className={`column fs-01 cursor-pointer`} onClick={()=>clickHandler()}>
+                <span className={`text-orange`}>{t("Campaign.title")}</span>
+                <span className={`mt-05 ${classes.flashit}`}>{t("Campaign.content")}</span>
+            </div>}
             <div className={`column`}>
-                <div className={`row jc-start ai-center my-1 ${classes.flashit}`}>
+                <div className={`row jc-start ai-center my-1`}>
                     <span className={`${classes.arrow} ml-05`}></span>
                     <Link to={Panel} className={`mr-05 cursor-pointer hover-text hover-scale-01`}>{t("MarketTitle.advancedTrading")}</Link>
                 </div>
